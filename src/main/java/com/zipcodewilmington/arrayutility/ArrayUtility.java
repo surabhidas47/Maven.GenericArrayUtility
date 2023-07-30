5d1ab6dcd6e47569867f2400ab551e0f4653a0e9
surabhi <surabhi.das@outlook.com>
2023-07-30 10:51:45 -0400

    starting lab

--- src/main/java/com/zipcodewilmington/arrayutility/ArrayUtility.java
+++ src/main/java/com/zipcodewilmington/arrayutility/ArrayUtility.java
@@ -3,5 +3,17 @@ package com.zipcodewilmington.arrayutility;
 /**
  * Created by leon on 3/6/18.
  */
-public class ArrayUtility {
+public class ArrayUtility <T>{
+
+    private T[] array;
+
+    ArrayUtility(T [] givenArray){
+        this
+    }
+
+
+
+//    public Integer countDuplicatesInMerge(Integer[] arrayToMerge, Integer valueToEvaluate) {
+//
+//    }
 }
