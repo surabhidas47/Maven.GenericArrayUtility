30dd5d178d62765b9fb199be7ca610ba5c17135b
surabhi <surabhi.das@outlook.com>
2023-08-02 22:09:17 -0400

    starting over

--- src/main/java/com/zipcodewilmington/arrayutility/ArrayUtility.java
+++ src/main/java/com/zipcodewilmington/arrayutility/ArrayUtility.java
@@ -6,108 +6,25 @@ import java.util.Arrays;
 /**
  * Created by leon on 3/6/18.
  */
-public class ArrayUtility <T>{
+public class ArrayUtility <T> {
 
     //creating generic array so any kind of given string/integer array will work
-    private T[] array;
+    private T[] givenArray;
 
     //constructor
-    ArrayUtility(T [] givenArray){
+    ArrayUtility(T[] givenArray) {
 
-        this.array = givenArray;
+        this.givenArray = givenArray;
     }
 
-    //method to merge arrays for count duplicates and get most common
+    //first creating method to merge arrays
 
-    public void mergeArray(T[] arrayToMerge){
+    private void mergeArrays(T[] arrayToMerge) {
 
-        T [] newArray = Arrays.copyOf(array, array.length+arrayToMerge.length);
+        //creating a copy of our givenArray, standard to do this
+        T [] copyInto = Arrays.copyOf(givenArray,givenArray.length+arrayToMerge.length);
+        //copying arrayToMerge into bothA
 
-
-        System.arraycopy(arrayToMerge,0,newArray, array.length, arrayToMerge.length);
-
-
-
-
-
-//        T [] newArray = (T[]) Array.newInstance(arrayToMerge[0].getClass(),array.length+arrayToMerge.length);
-//
-//        //orginal array into new array
-//
-//        for (int i=0; i<array.length;i++){
-//            newArray[i]=array[i];
-//        }
-//
-//        //iterating from array.length to array.length + arrayToMerge.length
-//        //adding mergearray from array.length(gives us the starting index to begin adding into
-//        for (int i= array.length;i< array.length+arrayToMerge.length;i++){
-//
-//            newArray[i]=arrayToMerge[i-array.length];
-//        }
-        this.array=newArray;
-
-    }
-
-
-    //methods generated from test
-
-
-    public Integer getNumberOfOccurrences(T valueToEvaluate) {
-        int counter =0;
-        for (int i=0; i<array.length;i++){
-            if (array[i].equals(valueToEvaluate)){
-                counter++;
-            }
-        }
-        return counter;
-    }
-
-    public T[] removeValue(T valueToRemove) {
-
-        int counter =0;
-
-        Integer remove = (Integer) getNumberOfOccurrences(valueToRemove);
-        //T [] newArray = Arrays.copyOf(array, array.length-remove);
-        T [] newArray = (T[]) Array.newInstance(valueToRemove.getClass(),array.length-remove);
-
-        for (int i=0; i<array.length;i++){
-            if (!array[i].equals(valueToRemove)){
-                newArray[counter]=array[i];
-            }
-        }
-
-        return newArray;
-    }
-
-
-    public Integer countDuplicatesInMerge(Integer[] arrayToMerge, T valueToEvaluate) {
-
-//        T [] newArray = mergeArray((T[]) arrayToMerge);
-//        this.array = newArray;
-
-        int duplicate = (int) getNumberOfOccurrences(valueToEvaluate);
-        return duplicate;
-    }
-
-    public T getMostCommonFromMerge(T[] arrayToMerge){
-//        T [] newArray = mergeArray(arrayToMerge);
-//
-//        int mostCommon =0;
-//        T commonObj = null;
-//        int counter =0;
-//
-//        for (int i=0;i< newArray.length;i++){
-//            for (int j=0;i< newArray.length;j++){
-//                if(newArray[i]==newArray[j]){
-//                    counter++;
-//                }
-//            }
-//            if (counter>mostCommon){
-//                mostCommon=counter;
-//                commonObj=newArray[i];
-//            }
-//        }
-        return null;//commonObj;
     }
 
 
