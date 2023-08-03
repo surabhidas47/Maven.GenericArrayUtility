57e1937d0ed872e3e74b7680c5e8c0951f4e4550
surabhi <surabhi.das@outlook.com>
2023-08-02 22:52:35 -0400

    idk how to test as i make the method, they have red in them

--- src/main/java/com/zipcodewilmington/arrayutility/ArrayUtility.java
+++ src/main/java/com/zipcodewilmington/arrayutility/ArrayUtility.java
@@ -22,10 +22,62 @@ public class ArrayUtility <T> {
     private void mergeArrays(T[] arrayToMerge) {
 
         //creating a copy of our givenArray, standard to do this
+
         T [] copyInto = Arrays.copyOf(givenArray,givenArray.length+arrayToMerge.length);
+
         //copying arrayToMerge into bothA
+        //System.arraycopy() copies the array contents from the
+        // (source array,
+        // beginning at the specified position of source array,
+        // array to copied into
+        //starting position of destination array
+        //length of to total to copy
+
+        System.arraycopy(arrayToMerge,0,copyInto,givenArray.length,arrayToMerge.length);
+        this.givenArray= copyInto;
+    }
+
+    public Integer getNumberOfOccurrences(T valueToEvaluate) {
+
+        int count =0;
+
+        for (int i=0;i< givenArray.length;i++){
+            if(valueToEvaluate.equals(givenArray[i]));
+            count++;
+        }
+
+        return count;
 
     }
 
+    public T getMostCommonFromMerge(Integer[] arrayToMerge) {
+        //need to return most common object
 
+        //first use mergeArrays method
+        mergeArrays((T[]) arrayToMerge);
+
+        int mostCommonNum =0;
+        T mostCommonObj = null;
+
+        for (int i=0;i<givenArray.length;i++){
+            //storing iteration in current value
+            T currentIter = givenArray[i];
+
+            //using getNumOfOccurances
+            //know to store in int bc thats what we return in this method
+            int count =getNumberOfOccurrences(currentIter);
+
+
+            // updates the mostCommonValue and mostCommon variables whenever
+            // a new maximum count is found during the loop iteration
+            if(count>mostCommonNum){
+                mostCommonNum=count;
+                mostCommonObj=currentIter;
+            }
+
+
+        }
+
+        return mostCommonObj;
+    }
 }
